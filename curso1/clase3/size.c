4065ef86ee21b11c5d64c4ce6c5ef0eb071f029e
SaReBenu <benulego0505@yahoo.com>
2018-10-23 17:38:25 +0200

    beautified

--- curso1/clase3/size.c
+++ curso1/clase3/size.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-  printf("size of char:%lu in bits:%lu \n", sizeof(char), 8*sizeof(char));
-  printf("size of short:%lu in bits:%lu \n", sizeof(short), 8*sizeof(short));
-  printf("size of int:%lu in bits:%lu \n", sizeof(int), 8*sizeof(int));
-  printf("size of long:%lu in bits:%lu \n", sizeof(long), 8*sizeof(long));
-  printf("size of float:%lu in bits:%lu \n", sizeof(float), 8*sizeof(float));
-  printf("size of double:%lu in bits:%lu \n", sizeof(double), 8*sizeof(double));
+int main() {
+  printf("size of char:%lu in bits:%lu \n", sizeof(char), 8 * sizeof(char));
+  printf("size of short:%lu in bits:%lu \n", sizeof(short), 8 * sizeof(short));
+  printf("size of int:%lu in bits:%lu \n", sizeof(int), 8 * sizeof(int));
+  printf("size of long:%lu in bits:%lu \n", sizeof(long), 8 * sizeof(long));
+  printf("size of float:%lu in bits:%lu \n", sizeof(float), 8 * sizeof(float));
+  printf("size of double:%lu in bits:%lu \n", sizeof(double),
+         8 * sizeof(double));
   return 0;
 }
