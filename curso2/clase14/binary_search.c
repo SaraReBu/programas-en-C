981606b5e4ccdf8b5388cfd8f52cac079b2e22cb
SaReBenu <benulego0505@yahoo.com>
2019-03-05 17:32:50 +0100

    binary search complete

--- curso2/clase14/binary_search.c
+++ curso2/clase14/binary_search.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
-int binary_search(int item);
+int binary_search(char item);
 
-int list[] = {1, 3, 5, 7, 9};
+char list[] = {'b', 'c', 'd', 'e'};
 
 int main() {
-  printf("%d\n", binary_search(1));
-  printf("%d\n", binary_search(3));
-  printf("%d\n", binary_search(2));
-  printf("%d\n", binary_search(9));
+  printf("%d\n", binary_search('b'));
+  printf("%d\n", binary_search('c'));
+  printf("%d\n", binary_search('a'));
+  printf("%d\n", binary_search('d'));
+  printf("%d\n", binary_search('e'));
+  printf("%d\n", binary_search('g'));
+}
+
+int binary_search(char item) {
+  int low = 0;
+  int high = (sizeof(list) / sizeof(char)) - 1;
+  while (low <= high) {
+    int mid = (high + low) / 2;
+    char guess = list[mid];
+    if (item == guess) {
+      return mid;
+    }
+    if (item < guess) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
 }
