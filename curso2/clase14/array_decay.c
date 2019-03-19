dc207bf356a302c034724d0f01a4ec6f08178541
SaReBenu <benulego0505@yahoo.com>
2019-03-19 17:15:30 +0100

    array decay complete

--- curso2/clase14/array_decay.c
+++ curso2/clase14/array_decay.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
-int binary_search(int item, int list[]);
+int binary_search(int item, int list[], int size);
 
-void printfbinary_search(int c, int list[]);
+void printfbinary_search(int c, int list[], int size);
 
 int main() {
 
   int list1[] = {1, 2, 3, 4};
-  printfbinary_search(0, list1);
-  printfbinary_search(1, list1);
-  printfbinary_search(2, list1);
-  printfbinary_search(3, list1);
-  printfbinary_search(4, list1);
-  printfbinary_search(5, list1);
+  printfbinary_search(0, list1, sizeof(list1));
+  printfbinary_search(1, list1, sizeof(list1));
+  printfbinary_search(2, list1, sizeof(list1));
+  printfbinary_search(3, list1, sizeof(list1));
+  printfbinary_search(4, list1, sizeof(list1));
+  printfbinary_search(5, list1, sizeof(list1));
 }
 
-int binary_search(int item, int list[]) {
+int binary_search(int item, int list[], int size) {
   int low = 0;
-  int high = (sizeof(list) / sizeof(int)) - 1;
-
-  printf("high: %d\n", high);
+  // size of the  list =tamaño de un puntero=8
+  // ARRAY_DECAY
+  // array_decay.c:21:21: warning: 'sizeof' on array function parameter 'list'
+  // will return size of 'int *' [-Wsizeof-array-argument]
+  //    int high = (sizeof(list) / sizeof(int)) - 1;
+  int high = (size / sizeof(int)) - 1;
 
   while (low <= high) {
     int mid = (high + low) / 2;
@@ -36,8 +39,9 @@ int binary_search(int item, int list[]) {
   return -1;
 }
 
-void printfbinary_search(int c, int list[]) {
-  printf(binary_search(c, list) >= 0 ? "%d se encuentra en la posicion %d\n"
-                                     : "%d no se encuentra en list\n",
-         c, binary_search(c, list));
+void printfbinary_search(int c, int list[], int size) {
+  printf(binary_search(c, list, size) >= 0
+             ? "%d se encuentra en la posicion %d\n"
+             : "%d no se encuentra en list\n",
+         c, binary_search(c, list, size));
 }
