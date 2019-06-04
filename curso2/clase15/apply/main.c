f55f59f94e569be6c5e03ddeb8c40bc10ba721dd
SaReBenu <benulego0505@yahoo.com>
2019-06-04 17:24:26 +0200

    apply finished

--- curso2/clase15/apply/main.c
+++ curso2/clase15/apply/main.c
@@ -2,29 +2,58 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-list_t apply(list_t, void *(*)(void *));
+list_t apply(list_t, void *(*)(const void *));
 
-void * capitalize(void *);
+void *capitalize_naive(const void *);
 
-void printflist(list_t list) {
-  for (int i = 0; i < count(list); i++) 
+void *capitalize(const void *);
+
+void printflist(char *msg, list_t list)
+{
+  printf("%s\n", msg);
+  for (int i = 0; i < count(list); i++)
   {
     printf("%s\n", item_at(list, i));
   }
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
   list_t list = NULL;
-  list = append(list, "a");
+  list = append(list, "A");
   list = append(list, "b");
   list = append(list, "c");
-  printflist(list);
-  list_t capitalized=NULL;
-  capitalized=apply(list,capitalize);
-  printflist(capitalize);
+  printflist("Original", list);
+  list_t capitalized = NULL;
+  capitalized = apply(list, capitalize_naive);
+  printflist("capitalized", capitalized);
+
+  capitalized = apply(list, capitalize);
+  printflist("capitalized :)", capitalized);
 }
 
-void * capitalize(void* src){
-  void *dst=malloc(strlen(src));
+void *capitalize_naive(const void *src)
+{
+  size_t len = strlen(src);
+  char *dst = malloc(len + 1);
+  strcpy(dst, src);
+  for (int i = 0; i < len; i++)
+  {
+    dst[i] = dst[i] - 32;
+  }
+  return dst;
+}
+
+void *capitalize(const void *src)
+{
+  size_t len = strlen(src);
+  char *dst = malloc(len + 1);
+  strcpy(dst, src);
+  for (int i = 0; i < len; i++)
+  {
+    dst[i] = toupper(dst[i]);
+  }
+  return dst;
 }
