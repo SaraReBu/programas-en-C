a4d25cbc5557eed44b0894ca32799d68bd3db139
SaReBenu <benulego0505@yahoo.com>
2019-11-12 17:32:46 +0100

    continue with base conberter

--- curso3/class4/base_converter.py
+++ curso3/class4/base_converter.py
@@ -2,4 +2,9 @@ import sys
 number = int(sys.argv[1])
 base = int(sys.argv[2])
 quotient = number / base
-while(){}
+l = list()
+while number != 0:
+    r = number % base
+    number = number // base
+    l.append(r)
+print(l)
