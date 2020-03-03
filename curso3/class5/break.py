ff4d84ec889803e19604755eaf82e731f2ff2f1c
SaReBenu <benulego0505@yahoo.com>
2020-03-03 17:26:47 +0100

    ej de break y while

---
+++ curso3/class5/break.py
@@ -0,0 +1,5 @@
+while respuesta := input("¿En qué año se publicó Python?"):
+    if respuesta == "1994":
+        print("La respuesta es correcta")
+        break
+    print("La respuesta no es correcta")
